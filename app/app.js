d6de80c3cd9a44652643f2dd9baf90b51f23d833
Jesús Enríquez <jesus@email.com>
2026-01-08 11:49:01 -0600

    Factorización en funciones en el archivo js

--- app/app.js
+++ app/app.js
@@ -34,23 +34,28 @@ function renderTareas() {
 const titulo = document.getElementById("titulo");
 const boton = document.getElementById("btnCambiar");
 
-boton.addEventListener("click", () => {
+boton.addEventListener("click", handleCambiarTexto);
+
+function handleCambiarTexto() {
   titulo.textContent = "Texto cambiado con JavaScript 🚀";
-});
+}
 
 const inputNombre = document.getElementById("nombre");
 const btnSaludar = document.getElementById("btnSaludar");
 const resultado = document.getElementById("resultado");
 
-btnSaludar.addEventListener("click", () => {
+btnSaludar.addEventListener("click", handleSaludar);
+
+function handleSaludar() {
   const nombre = inputNombre.value;
 
   if (nombre === "") {
     resultado.textContent = "Por favor escribe tu nombre";
-  } else {
+  } 
+  else {
     resultado.textContent = `Hola ${nombre}, bienvenido a Fullstack 90D 🚀`;
   }
-});
+}
 
 const inputTarea = document.getElementById("tarea");
 const btnAgregar = document.getElementById("btnAgregar");
